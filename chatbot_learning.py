10fb1405f282b275637856452839b4443d4037a8
Dev-Alexandros <hankes.alex@proton.me>
2025-02-04 11:14:12 -0600

    Create chatbot_learning.py
    Older learning shenagians

---
+++ chatbot_learning.py
@@ -0,0 +1,10 @@
+#Greeting for the user
+user_name = input("What is your name?  Input here and select ENTER key: ")
+print("Welcome to Alexander's Chatbot, " + user_name + ".")
+
+#Explains the bot's purpose to the user
+print("This program will repeat what you say.  Let's begin!")
+
+#echoes-based chat bot
+stuff_to_echo = input("Say something now: ")
+print ("You said: " + stuff_to_echo)
